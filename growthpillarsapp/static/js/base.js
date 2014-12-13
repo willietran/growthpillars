8f489cab60110a96fe2e138d670ef3cb679267d3
Ryan Rhee <ryan.y.rhee@gmail.com>
2014-12-13 15:46:03 -0800

    Indent base.js to be <80 chars

--- growthpillarsapp/static/js/base.js
+++ growthpillarsapp/static/js/base.js
@@ -4,65 +4,64 @@
 
 $(document).ready(function() {
 
-    function getCookie(name) {
-        var cookieValue = null;
-        if (document.cookie && document.cookie != '') {
-            var cookies = document.cookie.split(';');
-            for (var i = 0; i < cookies.length; i++) {
-                var cookie = jQuery.trim(cookies[i]);
-                // Does this cookie string begin with the name we want?
-                if (cookie.substring(0, name.length + 1) == (name + '=')) {
-                    cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                    break;
-                }
-            }
+  function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie != '') {
+      var cookies = document.cookie.split(';');
+      for (var i = 0; i < cookies.length; i++) {
+        var cookie = jQuery.trim(cookies[i]);
+        // Does this cookie string begin with the name we want?
+        if (cookie.substring(0, name.length + 1) == (name + '=')) {
+          cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+          break;
         }
-        return cookieValue;
+      }
     }
-    var csrftoken = getCookie('csrftoken');
+    return cookieValue;
+  }
+  var csrftoken = getCookie('csrftoken');
 
-    function csrfSafeMethod(method) {
+  function csrfSafeMethod(method) {
     // these HTTP methods do not require CSRF protection
-        return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+  }
+
+  $.ajaxSetup({
+    beforeSend: function(xhr, settings) {
+      if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+        xhr.setRequestHeader("X-CSRFToken", csrftoken);
+      }
     }
+  });
 
-    $.ajaxSetup({
-        beforeSend: function(xhr, settings) {
-            if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
-                xhr.setRequestHeader("X-CSRFToken", csrftoken);
-            }
-        }
+  console.log("ready");
+  $('.post-submit')[0].onclick = (function(e){
+    console.log("onclick handler");
+    e.preventDefault();
+    var url = '/post_submit/';
+    // Create a key value pairs to push into the model
+    var data = {
+      'title':$('#id_title')[0].value,
+      'place':$('#id_place')[0].value,
+      'idea':$('#id_idea')[0].value,
+      'link':$('#id_link')[0].value,
+      'labor':$('#id_labor')[0].value,
+      'audience':$('#id_audience')[0].value,
+      'result':$('#id_result')[0].value,
+      'spend':$('#id_spend')[0].value,
+      'story':$('#id_story')[0].value
+    };
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: data,
+      success: function(response) {
+        console.log(response);
+      },
+      error: function(response) {
+        console.log("No good!");
+        console.log(response);
+      }
     });
-
-    console.log("ready");
-    $('.post-submit')[0].onclick = (function(e){
-        console.log("onclick handler");
-        e.preventDefault();
-        var url = '/post_submit/';
-        // Create a key value pairs to push into the model
-        var data = {
-            'title':$('#id_title')[0].value,
-            'place':$('#id_place')[0].value,
-            'idea':$('#id_idea')[0].value,
-            'link':$('#id_link')[0].value,
-            'labor':$('#id_labor')[0].value,
-            'audience':$('#id_audience')[0].value,
-            'result':$('#id_result')[0].value,
-            'spend':$('#id_spend')[0].value,
-            'story':$('#id_story')[0].value
-        };
-    //    var dataInput = JSON.stringify(data);
-        $.ajax({
-            url: url,
-            type: "POST",
-            data: data,
-            success: function(response) {
-                console.log(response);
-            },
-            error: function(response) {
-                console.log("No good!");
-                console.log(response);
-            }
-        });
-    });
+  });
 });
