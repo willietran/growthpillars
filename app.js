dd2cd9d804533d5016003d2c9b5a82f6c72ba44a
Ryan Rhee <ryan.y.rhee@gmail.com>
2015-01-27 01:15:50 -0800

    [server] Break out auth into its own router

--- app.js
+++ app.js
@@ -5,53 +5,13 @@ var express = require('express')
   , reactify = require('reactify')
   , sass = require('node-sass-middleware')
   , nunjucks = require('nunjucks')
-  , passport = require('passport')
-  , TwitterStrategy = require('passport-twitter').Strategy
+  , AuthModule = require('./server/auth')
   , backend = require('./server/backend')
 ;
 
 var server = function(err) {
   // User model from mongodb
   var User = backend.user;
-
-  // Passport session setup.
-  passport.serializeUser(function(user, done) {
-    done(null, user.id);
-  });
-
-  passport.deserializeUser(function(id, done) {
-    User.findById(id, function (err, user) {
-      done(err, user);
-    });
-  });
-
-  // Twitter OAuth
-  passport.use(new TwitterStrategy(
-    {
-      consumerKey: 'fh03JiplqSKfQVFoVbsq3VAVv',
-      consumerSecret: 'HT2pxkUCZtUtqT4JwuynE5OW6JHM34ooC2qHxobaKW4vlJoCgE',
-      callbackURL: "http://127.0.0.1:3000/auth/twitter/callback"
-    },
-    function(token, tokenSecret, profile, done) {
-      var name = profile.name;
-      // object to be used to uniquely identify a user
-      var findObj = {
-        twitter_id: profile.id,
-      };
-      // additional info for user being created, if a user is not found
-      var moreInfo = {
-        name: profile.displayName,
-      };
-      // only set profile photo if supplied
-      if (profile.photos.length > 0) {
-        moreInfo.image_url = profile.photos[0].value;
-      }
-      User.findOrCreate(findObj, moreInfo, function(err, user) {
-        if (err) { return done(err); }
-        done(null, user);
-      });
-    }
-  ));
 
   // initialise express
   var app = express();
@@ -81,15 +41,17 @@ var server = function(err) {
     resave: false,
     saveUninitialized: false,
   }));
-  // initialize passport
-  app.use(passport.initialize());
-  // use passport.session() middleware to support persistent login sesisons
-  app.use(passport.session());
+  
+  var auth = AuthModule(User);
+  auth.setupSession(app);
 
   // use nunjucks to process view templates in express
   nunjucks.configure('server/templates/views', {
     express: app
   });
+
+  // our auth module
+  app.use('/auth', auth.router);
 
   /*
     set up any additional server routes (api endpoints, static pages, etc.)
@@ -124,24 +86,6 @@ var server = function(err) {
 
   app.get('/view/:post_id', function(req, res) {
     res.render('view.html', backend.fake_posts[0]);
-  });
-
-  // Redirect the user to Twitter for authentication.  When complete, Twitter
-  // will redirect the user back to the application at
-  //   /auth/twitter/callback
-  app.get('/auth/twitter', passport.authenticate('twitter'));
-
-  // Twitter will redirect the user to this URL after approval.  Finish the
-  // authentication process by attempting to obtain an access token.  If
-  // access was granted, the user will be logged in.  Otherwise,
-  // authentication has failed.
-  app.get('/auth/twitter/callback',
-    passport.authenticate('twitter', { successRedirect: '/',
-                                       failureRedirect: '/login' }));
-
-  app.all('/logout', function(req, res) {
-    req.logout();
-    res.redirect('/');
   });
 
   // start the server
