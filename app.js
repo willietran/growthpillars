032b8994a890d19d278fcb3a6ae49926ac3b1780
Ryan Rhee <ryan.y.rhee@gmail.com>
2015-01-29 19:53:04 -0800

    [server] Break out top-level router

--- app.js
+++ app.js
@@ -1,95 +1,22 @@
 var express = require('express')
-  , session = require('express-session')
-  , cookieParser = require('cookie-parser')
-  , bodyParser = require('body-parser')
-  , sass = require('node-sass-middleware')
   , nunjucks = require('nunjucks')
-  , AuthModule = require('./server/auth')
-  , backend = require('./server/backend')
+  , routerAsync = require('./server/router')
 ;
 
-var server = function(err) {
-  // User model from mongodb
-  var User = backend.user;
-
-  // initialise express
+routerAsync(function(router) {
+  // initialize express
   var app = express();
-  var isProd = app.get('env') === 'production';
-
-  // live reload, only on dev mode
-  if (!isProd) {
-    console.log('Using live reload');
-    app.use(require('connect-livereload')());
-  }
-
-  // sass will automatically compile matching requests for .css files
-  app.use(sass({
-    src: __dirname + '/private',
-    dest: __dirname + '/public',
-    debug: isProd ? false : true,
-    outputStyle: isProd ? 'compressed' : 'expanded',
-  }));
-  // public assets are served before any dynamic requests
-  app.use(express.static('public'));
-
-  app.use(cookieParser());
-  app.use(bodyParser.json());
-  app.use(bodyParser.urlencoded({ extended: true }));
-  app.use(session({
-    secret: 'sUp3Rs3Cr3t!',
-    resave: false,
-    saveUninitialized: false,
-  }));
-  
-  var auth = AuthModule(User);
-  auth.setupSession(app);
 
   // use nunjucks to process view templates in express
   nunjucks.configure('server/templates/views', {
     express: app
   });
 
-  // our auth module
-  app.use('/auth', auth.router);
-
-  /*
-    set up any additional server routes (api endpoints, static pages, etc.)
-    here before the catch-all route for index.html below.
-  */
-
-  app.get('/', function(req, res) {
-    // This used to be '*'.
-    //
-    // this route will respond to all requests with the contents of your index
-    // template. Doing this allows react-router to render the view in the app.
-    res.render('base.html', { posts: backend.fake_posts, user: req.user});
-  });
-
-  app.post('/post', function(req, res) {
-    if (req.user) {
-      var formData = req.body;
-      formData['user'] = req.user;
-      backend.addPost(formData, function (err, result) {
-        if (err) {
-          res.status(500).send({error: err});
-        } else {
-          res.send(result);
-        }
-      });
-    } else {
-      // Got create post request but user is not logged in. This shouldn't be
-      // possible from the web UI, so it must be a manually crafted request.
-      res.sendStatus(403); // Forbidden
-    }
-  });
-
-  app.get('/view/:post_id', function(req, res) {
-    res.render('view.html', backend.fake_posts[0]);
-  });
+  // use top-level app router
+  app.use(router);
 
   // start the server
   var server = app.listen(process.env.PORT || 3000, function() {
     console.log('\nServer ready on port %d\n', server.address().port);
   });
-};
-backend.initialize(server);
+});
---
+++ server/router.js
@@ -0,0 +1,94 @@
+var express = require('express')
+  , session = require('express-session')
+  , cookieParser = require('cookie-parser')
+  , bodyParser = require('body-parser')
+  , sass = require('node-sass-middleware')
+  , nunjucks = require('nunjucks')
+  , AuthModule = require('./auth')
+  , backend = require('./backend')
+;
+
+var server = function(err) {
+  var router = express.Router();
+
+  // User model from mongodb
+  var User = backend.user;
+
+  var isProd = router.get('env') === 'production';
+
+  // live reload, only on dev mode
+  if (!isProd) {
+    console.log('Using live reload');
+    router.use(require('connect-livereload')());
+  }
+
+  // sass will automatically compile matching requests for .css files
+  router.use(sass({
+    src: __dirname + '/private',
+    dest: __dirname + '/public',
+    debug: isProd ? false : true,
+    outputStyle: isProd ? 'compressed' : 'expanded',
+  }));
+  // public assets are served before any dynamic requests
+  router.use(express.static('public'));
+
+  router.use(cookieParser());
+  router.use(bodyParser.json());
+  router.use(bodyParser.urlencoded({ extended: true }));
+  router.use(session({
+    secret: 'sUp3Rs3Cr3t!',
+    resave: false,
+    saveUninitialized: false,
+  }));
+
+  // our auth module
+  var auth = AuthModule(User);
+  var authRouter = auth.router;
+  var setupSession = auth.setupSession;
+  setupSession(router);
+  router.use('/auth', authRouter);
+
+  /*
+    set up any additional server routes (api endpoints, static pages, etc.)
+    here before the catch-all route for index.html below.
+  */
+
+  router.get('/', function(req, res) {
+    // This used to be '*'.
+    //
+    // this route will respond to all requests with the contents of your index
+    // template. Doing this allows react-router to render the view in the app.
+    res.render('base.html', { posts: backend.fake_posts, user: req.user});
+  });
+
+  router.post('/post', function(req, res) {
+    if (req.user) {
+      var formData = req.body;
+      formData['user'] = req.user;
+      backend.addPost(formData, function (err, result) {
+        if (err) {
+          res.status(500).send({error: err});
+        } else {
+          res.send(result);
+        }
+      });
+    } else {
+      // Got create post request but user is not logged in. This shouldn't be
+      // possible from the web UI, so it must be a manually crafted request.
+      res.sendStatus(403); // Forbidden
+    }
+  });
+
+  router.get('/view/:post_id', function(req, res) {
+    res.render('view.html', backend.fake_posts[0]);
+  });
+
+  return router;
+};
+
+module.exports = function(callback) {
+  backend.initialize(function(err) {
+    var router = server(err);
+    callback(router);
+  });
+};
