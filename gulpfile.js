7a3c8fb3631fcc05397b7ed573d2a376f381345f
Ryan Rhee <ryan.y.rhee@gmail.com>
2015-02-16 09:37:35 -0800

    [gulp] LiveReload support

--- gulpfile.js
+++ gulpfile.js
@@ -6,8 +6,10 @@ var buffer = require('vinyl-buffer');
 var watchify = require('watchify');
 var browserify = require('browserify');
 var open = require('gulp-open');
+var setupApp = require('./server/app.js');
 
 // Browserify
+// TODO(ryanrhee): use webpack instead of browserify to get react-hot-loader
 var bundler = watchify(browserify('./client/scripts/index.js', watchify.args));
 bundler.transform('6to5ify');
 // TODO(ryanrhee): Split out vendor bundle so incremental builds are faster
@@ -25,9 +27,41 @@ function bundle() {
 gulp.task('jsclient', bundle);
 bundler.on('update', bundle);
 
+// Live Reload
+var tinylr;
+gulp.task('livereload', function(callback) {
+  // standard LiveReload port
+  var port = 35729;
+  tinylr = require('tiny-lr')().listen(port, callback);
+});
+
+function notifyLiveReload(event) {
+  var fileName = require('path').relative(__dirname, event.path);
+
+  tinylr.changed({
+    body: {
+      files: [fileName]
+    }
+  });
+}
+
 // Express
 gulp.task('server', function(callback) {
-  // TODO(ryanrhee): Start express server with live reload configured
+  setupApp(function(app, callback) {
+    // live reload, only on dev mode
+    console.log('Using live reload');
+    app.use(require('connect-livereload')());
+    callback();
+  }, function(app) {
+    var server = app.listen(process.env.PORT || 3000, function() {
+      console.log('\nServer ready on port %d\n', server.address().port);
+    });
+  });
+});
+
+// Watch
+gulp.task('watch', function() {
+  // TODO(ryanrhee): Watch for static resource changes
 });
 
 // Default task
