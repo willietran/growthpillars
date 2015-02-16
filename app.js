c2fbd5e70ccb32a85e7e6ff1ffd9bd6d777c4b88
Ryan Rhee <ryan.y.rhee@gmail.com>
2015-02-16 09:15:22 -0800

    [server] Separate top-level app configuration & server startup

--- app.js
+++ app.js
@@ -1,22 +1,9 @@
-var express = require('express')
-  , nunjucks = require('nunjucks')
-  , routerAsync = require('./server/router')
-;
+var setupApp = require('./server/app.js');
 
-routerAsync(function(router) {
-  // initialize express
-  var app = express();
-
-  // use nunjucks to process view templates in express
-  nunjucks.configure('server/templates/views', {
-    express: app
-  });
-
-  // use top-level app router
-  app.use(router);
-
-  // start the server
+setupApp(null, function(app) {
+  // start an http server
   var server = app.listen(process.env.PORT || 3000, function() {
     console.log('\nServer ready on port %d\n', server.address().port);
   });
 });
+
