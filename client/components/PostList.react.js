c98691927b2e3f31bbeec7d5600324d9543f073f
Ryan Rhee <ryan.y.rhee@gmail.com>
2015-05-08 21:32:43 -0700

    PostItem component, PostList will consist of exterior styling + multiple PostItem elements

--- client/components/PostList.react.js
+++ client/components/PostList.react.js
@@ -4,6 +4,8 @@
 
 var React = require('react');
 
+var PostItem = require('./PostItem.react');
+
 var PostList = React.createClass({
   propTypes: {
     posts: React.PropTypes.array,
@@ -25,54 +27,11 @@ var PostList = React.createClass({
 
   _renderPosts: function() {
     return this.props.posts.map(function(post) {
-      var imgAlt = 'no image';
-      if (post.user != null && post.user.image_url != null) {
-        imgAlt = post.user.image_url;
-      }
       return (
-        <div className="posts-list">
-          <div className="row post-item" id={post.id}>
-            <div className="col-xs-10">
-              <div className="post-content-container">
-                <div className="upvote">
-                  <a href="#">
-                    <span className="glyphicon glyphicon-chevron-up"></span>
-                  </a>
-                  <span className="upvote">10</span>
-                </div>
-                <a className="post-title" href={'/view/' + post.id}>
-                  {post.title}
-                </a>
-                <p className="post-growth">
-                  {post.result + 'in' + post.labor + 'hours using' + post.place}
-                </p>
-              </div>
-            </div>
-            <div className="col-xs-2">
-              <div className="post-user-container">
-                <a
-                  href="#user-profile-link"
-                  title="Replace this with user's profile page">
-                  <img
-                    className="post-user-image img-circle"
-                    src={
-                      'data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAAC' +
-                      'H5BAAAAAAALAAAAAABAAEAAAICRAEAOw=='
-                    }
-                    alt={imgAlt}
-                  />
-                </a>
-                <a href="#">
-                  <span
-                    className="glyphicon glyphicon-comment"
-                    ariaHidden="true">
-                    {'10'}
-                  </span>
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
+        <PostItem
+          key={post.id}
+          {...post}
+        />
       );
     });
   }
